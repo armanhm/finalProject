93d053f353efd2467829b75637c0e73e12aeb551
armanhmi <armanhm@mail.com>
2019-05-21 13:54:09 +0430

    test4

--- src/com/company/Second.java
+++ src/com/company/Second.java
@@ -4,5 +4,6 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("salam");
         System.out.println("saa");
+        System.out.println("sdad");
     }
 }
